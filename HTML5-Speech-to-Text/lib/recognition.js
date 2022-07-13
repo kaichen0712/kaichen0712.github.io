c04e3bd1e3ab3c819525fb1eb41c410becf9950f
kaichen0712 <oreo88555@gmail.com>
2022-07-13 17:03:41 +0800

    first commit

--- HTML5-Speech-to-Text/lib/recognition.js
+++ HTML5-Speech-to-Text/lib/recognition.js
@@ -9,11 +9,11 @@ let recognition = new webkitSpeechRecognition()
 // set params
 recognition.continuous = false
 recognition.interimResults = true
-adjustStartSecond = -1
-adjustEndSecond = 1
+adjustStartSecond = -0.5
+adjustEndSecond = 0.5
 
 let pauseInterval = 30 * 60
-let pauseSeconds = 2
+let pauseSeconds = 3
 
 // start immediately
 // recognition.start();
