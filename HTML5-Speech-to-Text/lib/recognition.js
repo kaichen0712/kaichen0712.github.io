cb8accefe7205827472ef3c2a5b280205fa9ddbc
kaichen0712 <oreo88555@gmail.com>
2022-07-13 16:51:31 +0800

    first commit

--- HTML5-Speech-to-Text/lib/recognition.js
+++ HTML5-Speech-to-Text/lib/recognition.js
@@ -12,7 +12,7 @@ recognition.interimResults = true
 adjustStartSecond = -1
 adjustEndSecond = 2
 
-let pauseInterval = 30 * 20
+let pauseInterval = 30 * 60
 let pauseSeconds = 1
 
 // start immediately
